0e062db2dacf4de4f7590b47ae6434586ba5a535
Christopher McElvogue <chris.mcelvogue@gmail.com>
2021-12-20 02:03:46 -0800

    commit tree 7fc01089eedcacc78c302ef3595bae3e5648c00f

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+function findMatching(names, search) {
+  return names.filter(match => match.toLowerCase() === search.toLowerCase())
+}
+
+function fuzzyMatch(source, testString) {
+  return source.filter(
+    (possibleMatch) =>
+    possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+  );
+}
